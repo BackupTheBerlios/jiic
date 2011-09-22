85b0d70f1ddc3c17d8cba4a152511005892292e4
jamstah <jamstah>
2011-09-22 09:09:33 +0000

    Reflect new jar locations

--- jiic/perf.sh
+++ jiic/perf.sh
@@ -12,13 +12,13 @@ case $1 in
   "m")
     time mkisofs -o $DEST/$2-mkisofs.iso $2 2>/dev/null ;;
   "ijr")
-    time java -cp sabre.jar:iso9660.jar:. ISOtest $DEST/$2.iso --disable-eltorito $2 2>/dev/null ;;
+    time java -cp lib/sabre.jar:lib/iso9660.jar:. ISOtest $DEST/$2.iso --disable-eltorito $2 2>/dev/null ;;
   "ir")
-    time java -cp sabre.jar:iso9660.jar:. ISOtest $DEST/$2.iso --disable-eltorito --disable-joliet $2 2>/dev/null ;;
+    time java -cp lib/sabre.jar:lib/iso9660.jar:. ISOtest $DEST/$2.iso --disable-eltorito --disable-joliet $2 2>/dev/null ;;
   "ij")
-    time java -cp sabre.jar:iso9660.jar:. ISOtest $DEST/$2.iso --disable-eltorito --disable-rockridge $2 2>/dev/null ;;
+    time java -cp lib/sabre.jar:lib/iso9660.jar:. ISOtest $DEST/$2.iso --disable-eltorito --disable-rockridge $2 2>/dev/null ;;
   "i")
-    time java -cp sabre.jar:iso9660.jar:. ISOtest $DEST/$2.iso --disable-eltorito --disable-rockridge --disable-joliet $2 2>/dev/null ;;
+    time java -cp lib/sabre.jar:lib/iso9660.jar:. ISOtest $DEST/$2.iso --disable-eltorito --disable-rockridge --disable-joliet $2 2>/dev/null ;;
   *)
     echo "Usage: $0 mjr|mr|mj|m|ijr|ir|ij|i <file or directory>"
     echo "Example: $0 mjr eldream -> mkisofs; Joliet, Rock Ridge; eldream"
